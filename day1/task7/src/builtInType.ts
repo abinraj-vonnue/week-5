76af479be48f1e40737ebf2ef61f87b7d8f99ccf
Abin Raj <abinraj.mk@vonnue.com>
2026-07-28 12:10:41 +0530

    feat: Type declarations for third-party code
    - Demonstrated use of @types/node
    - wrote a declaration file : declaring a ficrional untyped module
    - Used satisfies operator
    - wrote ambient declaration

---
+++ day1/task7/src/builtInType.ts
@@ -0,0 +1,20 @@
+// src/builtInTypes.ts
+import { sendMessage } from "#some-library";
+sendMessage("Demonstrating log");
+
+// import * as http from "http";
+// const hostname = "127.0.0.1";
+
+// const port = 5002;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", "text/plain");
+//     res.end("Hello World");
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`server running at http://${hostname}:${port}`);
+// });
+
+// const palette = { primary: "#0D9488" } satisfies Record<string, string>;
